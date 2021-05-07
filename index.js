9527943a7910be16059e8dd698824a26bf246068
xatreyux <mwmailing@gmail.com>
2021-05-07 13:27:57 -0400

    added firebase Users collection

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+// require("firebase/auth");
+// require("firebase/firestore");
+const admin = require("firebase-admin");
+var serviceAccount = require("./creds.json");
+
+dotenv.config();
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://devport-express-backend.firebaseio.com",
+});
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    // credentials: true,
+  })
+);
+app.use("/auth", require("./routers/userRouter"));
---
+++ routers/userRouter.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
+var admin = require("firebase-admin");
+const firebase = require("firebase");
+var db = admin.firestore();
+
+router.post("/", async (req, res) => {
+  console.log("---User signup initiated---");
+  const { user, tokenId } = req.body;
+  console.log("User passed from frontend: ", user);
+  console.log("tokenId passed from frontend: ", tokenId);
+
+  admin
+    .auth()
+    .verifyIdToken(tokenId)
+    .then((decodedToken) => {
+      const uid = decodedToken.uid;
+      console.log("Fetched UID: ", uid);
+      console.log("Fetched email: ", user.user.email);
+
+      const Users = db.collection("users");
+      Users.doc(`${uid}`).set({
+        email: user.user.email,
+        posts: [],
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+module.exports = router;
